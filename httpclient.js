f5c60432c361363c7dd84420b88fb75438ff01af
Suguru FUJISAWA <udumge@gmail.com>
2016-06-13 15:39:11 +0900

    add event

--- httpclient.js
+++ httpclient.js
@@ -6,9 +6,13 @@ http.get("http://www.google.co.jp/", function(res){
   //console.log("Got response:"+ util.inspect(res,{showHidden: true, depth : null,colors: true}));
   console.log("Headers\r\n" + util.inspect(res.headers,{showHidden: true, depth : null,colors: true}));
   console.log("StatusCode\r\n" + res.statusCode);
+  var body = "";
+  res.on("data",function(data){
+    body += data;
+  });
   res.resume();
-  res.on("data", function(chunk){
-    console.log("Body \r\n" + chunk);
+  res.on("end", function(){
+    console.log("Body \r\n" + body);
   });
 }).on("error", function(e){
   console.log("Got Error! " + e.message);
